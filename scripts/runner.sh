a63acdac89dfa85f06e0be8e78f4dadcaa722fdb
Andy Fleming <andy@ulendo.com>
2025-03-17 21:18:12 +0200

    Update

---
+++ scripts/runner.sh
@@ -0,0 +1,11 @@
+nodemon --verbose \
+    --watch ../lib/ngonyePlantModel/model/plantModel.js \
+    --watch ../lib/ngonyePlantModel/model/powerAndEnergyModel.js \
+    --watch ../lib/ngonyePlantModel/model/flowsModel.js \
+    --watch ../lib/ngonyePlantModel/model/levelsAndLossesModel.js \
+    --watch ../lib/ngonyePlantModel/model/eFlowsModel.js \
+    --watch ../lib/ngonyePlantModel/model/statistics.js \
+    --watch ../data/ngonyePlantModels/modelParameters.js \
+    --watch -e js,csv \
+    ../lib/ngonyePlantModel/modelRunner.js &
+
