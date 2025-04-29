65fa4da21363f10ebd99a27267ad3991586e41d2
Andy Fleming <andy@ulendo.com>
2025-04-29 09:54:40 +0200

    Update

---
+++ data/ngonyePlantModels/cciaModelParameters.js
@@ -0,0 +1,61 @@
+export const baseParams = {
+  type: 'sh',
+  hydrologySet:'2024',
+  ewrCategorySet: 'Recommendation 1',
+  tailwaterLift: 0,
+  headpondLift: 0,
+  plantCapacity: 180,
+  unitsAvailable: 4,
+  minimumHead: 7.8,
+  maximumHead: 25.4,
+  maximumHeadShutdown: true,
+  minimumFlowUnit: 50,
+  maximumFlowUnit: 275,
+  ratedFlowUnit: 220,
+  ratedTurbineCapacity: 46.5,
+  maxGeneratorOutput: 48.2,
+  constrainFinalGeneratorOutput: true,
+  lookupsFileset: 'sh',
+  unitLimits: {
+    floodFlowCfs: [18.6, 65],
+    overloadCfs: [-10.2, 479]
+  }  
+}
+export const models = [
+  {
+    force: true,
+    modelRef:'sh_2024_7.8mhead',
+    modelName: 'Sinohydro Bid - Hydrology to 2024 - Minimum head 7.8m',
+    description: 'Parameters from Sinohydro bid with 2024 hydrology and minimum head of 7.8m',
+  },
+]
+
+export const lookupFilesets = [
+  {
+    ref: 'fs',
+    headlossCanal: 'headloss_canal_fs.csv',
+    hillchart: 'hillchart_fs.csv',
+    headlossLeftChannel: 'headloss_leftchannel.csv',
+    headlossTurbine: 'headloss_turbine_fs.csv',
+    tailwaterLevel: 'tailwater_levels.csv',
+    eFlowsAssuranceSets: 'eflows_assurance_sets.csv',
+    eFlowsChannelFlows: 'eflows_channel_flows.csv',
+    channelSpillProportions: 'channel_splill_proportions.csv',
+    spillLimits: 'spill_limits.csv',
+    generatorEfficiency: 'generator_efficiency.csv'
+  }, {
+    ref: 'sh',
+    headlossCanal: 'headloss_canal_sh.csv',
+    hillchart: 'hillchart_sh.csv',
+    headlossLeftChannel: 'headloss_leftchannel.csv',
+    headlossTurbine: 'headloss_turbine_sh.csv',
+    tailwaterLevel: 'tailwater_levels.csv',
+    eFlowsAssuranceSets: 'eflows_assurance_sets.csv',
+    eFlowsChannelFlows: 'eflows_channel_flows.csv',
+    channelSpillProportions: 'channel_splill_proportions.csv',
+    spillLimits: 'spill_limits.csv',
+    generatorEfficiency: 'generator_efficiency.csv',
+
+  },
+  
+]
