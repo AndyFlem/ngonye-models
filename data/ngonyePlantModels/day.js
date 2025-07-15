79fae5174a1c5179746fa8825400c5acc1b9fc4b
Andy Fleming <andy@ulendo.com>
2025-07-15 10:56:54 +0200

    Improved statistics for reporting

---
+++ data/ngonyePlantModels/day.js
@@ -0,0 +1,95 @@
+day={
+  date: '1924-10-01T00:00:00.000Z',
+  flow: 136.4602528773752,
+  FDCPercentile: 0.9975801116272822,
+  calMonthlyFDCPercentile: 0.98,
+  day: 275,
+  waterYear: 1924,
+  waterMonth: 1,
+  waterDay: 1,
+  waterWeek: 1,
+  ewrMeasureExceedance: 0.97,
+  ewrExceedance: 0.97,
+  ewrFlowBandNumber: 5,
+  ewrFlowBand: 'Dry',
+  datetime:  { ts: '1924-09-30T22:00:00.000+00:00', zone: 'Europe/London', locale: 'en-US' },
+  index: 0,
+  ewrMeasurementDate: -1428026400000,
+  flows: {
+    river: 136.4602528773752,
+    eFlows: {
+      channelA: 30,
+      channelC: 6,
+      channelD: 4,
+      channelE: 2,
+      channelFG: 30,
+      total: 72
+    },
+    eFlowProportion: 0.528,
+    availableGenerationFlow: 64.4603,
+    availableGenerationFlowProportion: 0.472,
+    canal: 64.4603,
+    spill: {
+      total: -0.0000471226,
+      channelA: -0.000010649707600000001,
+      channelC: -0.000001413678,
+      channelD: -0.0000014372393,
+      channelE: -2.000001413678,
+      channelFG: 1.9999677917028997
+    },
+    channels: {
+      channelA: 29.9999893502924,
+      channelC: 5.999998586322,
+      channelD: 3.9999985627607,
+      channelE: -0.0000014136779999951443,
+      channelFG: 31.9999677917029,
+      left: 104.4602864993751
+    }
+  },
+  levels: {
+    headpond: 990,
+    tailwaterLevel: 964.5,
+    grossHead: 25.5,
+    headlosses: {
+      leftchannel: 0.07239852126208914,
+      canal: 0.0012291255000000004,
+      upstream: 0.07362764676208913,
+      upstreamProportion: 0.002887358696552515,
+      canalProportion: 0.000048201000000000015
+    }
+  },
+  generation: {
+    calc1: {
+      units: 1,
+      unitFlow: 64.4603,
+      headlossTurbine: 0.029521405000000008,
+      netHead: 25.396850948237912,
+      headlossTotal: 0.10314905176208913,
+      headlossProportion: 0.00404506085341526,
+      unitEfficiency: 0.8824178310250526,
+      unitPower: 14.167237266757907,
+      unitPowerFactor: 0.30467176917758937,
+      generatorEfficiency: 0.96,
+      totalEfficiency: 0.8471211177840504,
+      generatorPower: 13.60054777608759,
+      plantPower: 13.60054777608759,
+      plantEnergy: 326.41314662610216
+    },
+    calc2: {
+      units: 1,
+      unitFlow: 64.4603,
+      headlossTurbine: 0.029521405000000008,
+      netHead: 25.396850948237912,
+      headlossTotal: 0.10314905176208913,
+      headlossProportion: 0.00404506085341526,
+      unitEfficiency: 0.8824178310250526,
+      unitPower: 14.167237266757907,
+      unitPowerFactor: 0.30467176917758937,
+      generatorEfficiency: 0.96,
+      totalEfficiency: 0.8471211177840504,
+      generatorPower: 13.60054777608759,
+      plantPower: 13.60054777608759,
+      plantEnergy: 326.41314662610216
+    }
+  }
+}
